30aea05f19cbd3b506f495871f70284b8e56825a
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-12-31 16:13:57 +0530

    Basic structure

---
+++ student.js
@@ -0,0 +1,6 @@
+function submitStudentForm() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // TODO: Validate credentials and handle the login process
+}
